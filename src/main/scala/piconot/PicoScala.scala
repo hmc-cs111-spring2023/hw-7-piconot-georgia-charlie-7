75db01af277d260e0e37fed82fe6499206154b35
Georgia Klein <georgia.r.klein@gmail.com>
2023-03-07 04:24:12 +0000

    Our picobot language implementation

--- src/main/scala/piconot/PicoScala.scala
+++ src/main/scala/piconot/PicoScala.scala
@@ -4,35 +4,62 @@ import picolib._
 import picolib.maze._
 import picolib.semantics._
 import java.io.File
+import scala.collection.mutable.ListBuffer
 
-// enum Direction:
-//     case North, East, West, South
+// Create the rules list, to have rules added to it.
+val rules = ListBuffer.empty[Rule]
 
-// @main
-// def main() = 
-//     val maze = Maze("resources" + File.separator + "empty.txt")
-    // def run = object EmptyText extends TextSimulation(maze, rules.toList)
-
-    // var rules = Seq[Rule]
-class when(surr: String)(state: Int) {
-    def go(direction: MoveDirection)(newState: Int) =
-        Rule(
-            State(state.toString()),
-            Surroundings(c2d(surr(0)), c2d(surr(1)), c2d(surr(2)), c2d(surr(3))),
-            direction,
-            State(newState.toString())
-        )
+// Nice view of the rules for the user.
+def showRules : Unit = {
+    rules.map(println(_))
 }
 
-def c2d(c: Char) = c match
-    case '*' => Anything
-    case 'x' => Open
-    case _ => Blocked   // currently allows any character to block
+// Object used to create a new rules.
+object when {
+    // Initialize values.
+    // Note, these values will not be passed into a rule unless the functions are called out of order. 
+    var initState = State("0")
+    var surroundings = Surroundings(Anything, Anything, Anything, Anything)
+    var direction : MoveDirection = North
+    
+    // Get the current state.
+    def state(state: Int) = {
+        initState = State(state.toString())
+        this
+    }
 
+    // If the input has four characters, convert the four characters into a Surroundings object.
+    def at(surr: String) = {
+        if (surr.length != 4) then throw new IllegalArgumentException("surroundings argument has too few or too many values")
+        else {
+            surroundings = Surroundings(getDirection(surr(0)), getDirection(surr(1)), getDirection(surr(2)), getDirection(surr(3)))
+            this
+        }
+    }
 
-// having trouble getting imported
-// line 18 is giving trouble "expression does not take parameters"
-// everything commented out does not work- use ListBuffer?
-// create main function
-// user input for name of maze
-// maybe MAYBE error handling if we somehow have time
+    // Get the new direction.
+    def go(dir: MoveDirection) = {
+        direction = dir
+        this
+    }
+
+    // Get the final state and combine the given information to create a new rule. 
+    def become(state: Int) = {
+        rules += Rule(initState, surroundings, direction, State(state.toString()))
+    }
+
+    // Convert a char to a direction.
+    private def getDirection(c: Char) = c match
+        case '*' => Anything
+        case 'x' => Open
+        case 'B' => Blocked
+        case _ => throw new IllegalArgumentException("illegal surroundings argument")
+}
+
+// Run the picobot program.
+def picoRun(mazeName: String) : Unit = {
+    // Note, the user input for the maze file must include the folder where the maze file is.
+    val maze = Maze(mazeName)
+    object EmptyText extends TextSimulation(maze, rules.toList)
+    EmptyText
+}
